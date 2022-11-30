8be598a02ad62579ec90c3eefd4df038184bbd85
spsp8426 <spsp8426@naver.com>
2022-11-30 19:31:56 +0900

    add button hover,decoration none

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -11,7 +11,7 @@ function Home () {
         </div>
         <div className="hero-text">
             <h1>rustPlusPlus ~ Rust+ Discord Bot</h1>
-             <p>A NodeJS Discord Bot that uses the <a href="https://github.com/liamcottle/rustplus.js" target="_blank" rel="noreferrer">rustplus.js</a> library to utilize the power of the <a href="https://rust.facepunch.com/companion" target="_blank" rel="noreferrer">Rust+ Companion App</a> with additional Quality-of-Life features.</p>
+             <p className="link-texts">A NodeJS Discord Bot that uses the <a href="https://github.com/liamcottle/rustplus.js" className="link-text" target="_blank" rel="noreferrer">rustplus.js</a> library to utilize the power of the <a href="https://rust.facepunch.com/companion" className="link-text" target="_blank" rel="noreferrer">Rust+ Companion App</a> with additional Quality-of-Life features.</p>
             <Link to="/settings" className="show">
             Get Start
             </Link>
